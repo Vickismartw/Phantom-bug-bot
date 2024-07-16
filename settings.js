9508ea9a1778d67b8727fcef7e2a8f31a8331002
Vickismartw <168165250+Vickismartw@users.noreply.github.com>
2024-07-16 17:04:17 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,19 +8,19 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "2348108395270"
-global.ownername = "*Phantom+Hacker"
+global.ownernomer = "2348160141238"
+global.ownername = "*KILLER"
 global.ytname = "YT: phantom-hacks01"
 global.socialm = ""
 global.location = "India, Mizoram, Aizawl"
 
-global.ownernumber = '2348108395270'  //creator number
-global.ownername = 'Phantom+Hacker' //owner name
+global.ownernumber = '2348160141238'  //creator number
+global.ownername = 'KILLER' //owner name
 global.botname = 'xbug bot' //name of the bot
 
 //sticker details
-global.packname = 'Sticker By Phantom'
-global.author = 'Hallo\n\nContact: 2348108395270'
+global.packname = 'Sticker By KILLER'
+global.author = 'Hallo\n\nContact: 2348160141238'
 
 //console view/theme
 global.themeemoji = '😡'
@@ -35,7 +35,7 @@ global.prefa = ['','!','.','#','&']
 //false=disable and true=enable
 global.autoRecording = false //auto recording
 global.autoTyping = false //auto typing
-global.autorecordtype = false //auto typing + recording
+global.autorecordtype =true//auto typing + recording
 global.autoread = false //auto read messages
 global.autobio = false //auto update bio
 global.anti92 = false //auto block +92 
